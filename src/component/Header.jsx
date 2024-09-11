4c055a8efc733d9eab8a8faf0fb45bc58157d747
HUY SAMA <huyprao71@gmail.com>
2024-09-11 15:49:47 +0700

    SIUUUUU

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,15 +4,16 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
+
         <header className="fixed w-full min-h-20 max-h-20">
-            <div className="container w-full min-h-20 max-h-20 flex justify-center pt-3 pb-1 mx-auto">
+            <div className="container w-full min-h-20 max-h-20 flex justify-center pt-3 pb-1 mx-auto drop-shadow-xl">
                 <nav className="flex items-center justify-between p-2 lg:px-8 border-4 rounded-xl border-transparent bg-[#F9C791] w-full drop-shadow-lg">
                     <div className="justify-start max-w-[120px]">
                         <a href="/" className="max-w-[120px]">
                             <img src="../fac_1.png" alt="" className="object-cover" />
                         </a>
                     </div>
-                    <div className="h-full mr-auto lg:flex justify-center">
+                    <div className="h-full mr-auto lg:flex justify-center font">
                         <Link
                             to="/home"
                             className="text-xl flex items-center justify-center font-semibold leading-6 text-black w-[125px]"
@@ -33,21 +34,44 @@ export default function Header() {
                         </Link>
                         <Link
                             to="/about"
-                            className="text-xl flex items-center justify-center font-semibold leading-6 text-black w-[125px]"
+                            className=" text-xl flex items-center justify-center font-semibold leading-6 text-black w-[125px]"
                         >
                             Nhân sự
                         </Link>
                     </div>
+
                     <div className=" justify-end ">
-                        <button className="flex bg-blue-600 rounded-lg px-1 py-1 w-36 space-x-2 items-center justify-center">
-                            <FaDiscord className="text-white jutsify-center text-xl" />
-                            <a
-                                href="https://discord.gg/PMVTHDgerp"
-                                className="text-lg font-semibold leading-6 text-white justify-center"
-                            >
-                                Join Discord
+                        <button className="flex w-36 items-center justify-between">
+                            <img className="size-1/4" src="../Youtube_MSOC.png" alt="DC_Logo" />
+                            <a href="https://discord.gg/PMVTHDgerp">
                             </a>
                         </button>
+                    </div>
+
+                    <div className=" justify-end ">
+                        <button className="flex w-36 space-x-1 items-center justify-between">
+                            <img className="size-1/4" src="../x_MSOC.png" alt="DC_Logo" />
+                            <a href="https://discord.gg/PMVTHDgerp">
+                            </a>
+                        </button>
+                    </div>
+
+                    <div className=" justify-end ">
+                        <button className="flex w-36 space-x-1 items-center justify-between">
+                            <img className="size-1/4" src="../DC_MSOC.png" alt="DC_Logo" />
+                            <a href="https://discord.gg/PMVTHDgerp">
+                            </a>
+                        </button>
+                    </div>
+
+
+                    <div className=" justify-end ">
+                        <button className="flex w-36 space-x-1 items-center justify-between">
+                            <img className="size-1/4" src="../FB_MSOC.png" alt="FB_Logo" />
+                            <a href="https://discord.gg/PMVTHDgerp">
+                            </a>
+                        </button>
+
                     </div>
                 </nav>
             </div>
